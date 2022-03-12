35d6b6061065998ab8e56f04daee30fe045b75c6
Christian <christian.k@netcom-mail.de>
2022-03-12 20:26:55 +0100

    Gamehandler init

--- Assets/Skripts/GameHandler.cs
+++ Assets/Skripts/GameHandler.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
 
 public class GameHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Variables to set in the untiy editor
+    // Buildable Prefabs
+    public GameObject buildableFlag;
+    public GameObject buildableHouse1;
+    public GameObject buildableHouse2;
+    public GameObject buildableHouse3;
+    public GameObject buildableHarbour;
 
-    // Update is called once per frame
-    void Update()
+    // Terrain
+    public Terrain activeTerrain;
+
+    // Textures
+    public Texture roadTexture;
+
+
+    // static global variables
+    // Buildable Prefabs
+    public static GameObject BuildableFlag;
+    public static GameObject BuildableHouse1;
+    public static GameObject BuildableHouse2;
+    public static GameObject BuildableHouse3;
+    public static GameObject BuildableHarbour;
+
+    // Terrain
+    public static Terrain ActiveTerrain;
+    public static TerrainData ActiveTerrainTerrainData;
+
+    // Textures
+    public static Texture RoadTexture;
+
+    private void Awake()
     {
-        
+        // Buildable Prefabs
+        BuildableFlag = buildableFlag;
+        BuildableHouse1 = buildableHouse1;
+        BuildableHouse2 = buildableHouse2;
+        BuildableHouse3 = buildableHouse3;
+        BuildableHarbour = buildableHarbour;
+
+        // Terrain
+        ActiveTerrain = activeTerrain;
+        ActiveTerrainTerrainData = ActiveTerrain.terrainData;
+
+        // Textures
+        RoadTexture = roadTexture;
     }
 }

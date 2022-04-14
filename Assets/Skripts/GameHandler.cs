474ba65320b75eacd1b1c50e4f44c8b4f1ab4dee
Bergschaf <christian.k@netcom-mail.de>
2022-04-14 20:41:10 +0200

    Building Types + Building UI

--- Assets/Skripts/GameHandler.cs
+++ Assets/Skripts/GameHandler.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UIElements;
--- Assets/Skripts/UI/BuildMenu/BuildMenuButtonScript.cs
+++ Assets/Skripts/UI/BuildMenu/BuildMenuButtonScript.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class BuildMenuButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int buttonID;
+    
+    public void Click()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
